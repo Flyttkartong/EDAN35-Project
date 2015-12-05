45ea4e1783e10f2b10b83325f1474c5f5ff4ec1c
Samuel Johansson <johansson.samuel@live.se>
2015-12-05 15:36:00 +0100

    Cleanup and change of camera orientation
    NOTE: If the points don't show up at first, just press the left mouse
    button and they should appear.
    
    - Changed camera orientation so that it starts off looking at the points
    - Disabled GLStateInspection console withing render window
    - Removed unused code
    - Removed some old comments

--- src/Testing.cpp
+++ src/Testing.cpp
@@ -46,11 +46,6 @@
 
 #define LIGHTS_NB 4
 
-
-static GLuint loadCone(GLuint& vboId, GLsizei& verticesNb, bonobo::ShaderProgram* shader);
-
-
-
 Testing::Testing(int argc, const char* argv[])
 {
 	Log::View::Init();
@@ -81,13 +76,6 @@ Testing::~Testing()
 
 void Testing::run()
 {
-	//Commit test
-
-	//
-	// Load the scene coming
-	//
-	
-
 	
 	//
 	// Compute the scene bounding box to scale the camera speed, near and far plane accordingly
@@ -96,55 +84,27 @@ void Testing::run()
 	float sceneScale = 10.0;
 
 	//
-	// Setup the camera
+	// Setup the camera. 
 	//
+	// NOTE: If the points don't show up at first, just press the left mouse button and they should appear. The camera orientation shouldn't need to be changed
+	// 
 	FPSCameraf mCamera = FPSCameraf(fPI / 4.0f, static_cast<float>(RES_X) / static_cast<float>(RES_Y), sceneScale * 0.01f, sceneScale * 4.0f);
-	mCamera.mWorld.SetTranslate(v3f(sceneScale * 0.17f, sceneScale * 0.03f, 0.0f));
-	mCamera.mRotation.x = fPI / 2.0f;
-	mCamera.mWorld.SetRotateY(fPI / 2.0f);
+	mCamera.mWorld.SetTranslate(v3f(0.f, 0.f, -2.f));//v3f(sceneScale * 0.17f, sceneScale * 0.03f, 0.0f));
+	mCamera.mRotation.x = fPI;// / 2.0f;
+	mCamera.mWorld.SetRotateY(0);//fPI / 2.0f);
 	mCamera.mMouseSensitivity = 0.003f;
 	mCamera.mMovementSpeed = sceneScale * 0.25f;
 
 
 	//
 	// Load all the shader programs used
+	std::string terrainShaderNames[2] = { SHADERS_PATH("terrain_gen.vert"),		SHADERS_PATH("terrain_gen.frag") };
+	bonobo::ShaderProgram *terrainShader = bonobo::loadShaderProgram(terrainShaderNames, 2);
 
-	std::string defaultShaderNames[2] = { SHADERS_PATH("default.vert"),           SHADERS_PATH("default.frag") };
-	std::string terrainShaderNames[2]		= { SHADERS_PATH("terrain_gen.vert"),		SHADERS_PATH("terrain_gen.frag") };
-
-	bonobo::ShaderProgram *defaultShader         = bonobo::loadShaderProgram(defaultShaderNames,   2);
-
-	bonobo::ShaderProgram *terrainShader		 = nullptr;
-
-	if (defaultShader == nullptr) {
-		LogError("Failed to load default shader\n");
+	if (terrainShader == nullptr) {
+		LogError("Failed to load terrain shader\n");
 		exit(-1);
 	}
-
-	auto reloadShaders = [&](){
-	
-		if (terrainShader != nullptr && terrainShader != defaultShader) {
-			bonobo::unloadShader(&terrainShader->mShaders[0]);
-			bonobo::unloadShader(&terrainShader->mShaders[1]);
-			bonobo::unloadShaderProgram(terrainShader);
-		}
-
-		terrainShader		  = bonobo::loadShaderProgram(terrainShaderNames,   2);
-
-		if (terrainShader == nullptr)
-			terrainShader = defaultShader;
-
-	};
-	reloadShaders();
-
-
-	//
-	// Setup VAOs, VBOs and IBOs for the scene and cone meshes
-	//
-	//Create VAO
-
-	//bonobo::VAO *meshLambertVao = bonobo::loadVertexAttributeObject(*lambertShader, *meshVbo, mesh->mVertexDesc);
-
 
 	float points[4*3] = {
 		-0.45f,  0.45f, 0.0f,
@@ -181,79 +141,8 @@ void Testing::run()
 	glBindBuffer(GL_ARRAY_BUFFER, 0u);
 	bonobo::checkForErrors();
 
-
-	glEnable(GL_DEPTH_TEST);
+	//glEnable(GL_DEPTH_TEST); // If this is enabled, the points don't show up
 	glEnable(GL_CULL_FACE);
-
-
-
-
-	/*
-	GLuint coneVao = 0u, coneVbo = 0u;
-	GLsizei coneVerticesNb = 0;
-	coneVao = loadCone(coneVbo, coneVerticesNb, spotlightShader);
-	*/
-
-
-	//
-
-	//bonobo::VAO *meshLambertVao = bonobo::loadVertexAttributeObject(*lambertShader, *meshVbo, mesh->mVertexDesc);
-
-	//
-	// Setup FBOs
-	//
-	//bonobo::Texture *testTex = bonobo::loadTexture2D(RES_X, RES_Y, bonobo::TEXTURE_UNORM, v4i(8, 8, 8, 8), MSAA_RATE);
-
-	//const bonobo::Texture *gbufferColorAttachments[1] = {testTex};
-
-//	bonobo::FBO *testingFbo = bonobo::loadFrameBufferObject(gbufferColorAttachments);
-
-	//
-	// Setup texture samplers
-	//
-	
-	
-
-	//GLfloat borderColor[4] = { 1.0f, 0.0, 0.0f, 0.0f };
-
-	
-
-
-	//
-	// Load the default texture
-	//
-	
-
-
-	//
-	// Setup lights properties
-	//
-	
-
-	//
-	// Setup some additionnal parameters
-	//
-	
-
-	//
-	// Set initial OpenGL state
-	//
-
-
-	//Own stuff
-	
-
-	/*// Specify layout of point data
-	GLint posAttrib = glGetAttribLocation(terrainShader->mId, "Vertex");
-	
-	glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(posAttrib);
-
-
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);*/
-
-	//end of own stuff
 
 
 	f64 ddeltatime;
@@ -276,18 +165,9 @@ void Testing::run()
 		inputHandler->Advance();
 		mCamera.Update(ddeltatime, *inputHandler);
 
-		if ((inputHandler->GetKeycodeState(GLFW_KEY_R) & JUST_PRESSED)) {
-			LogInfo("Reloading all shaders\n");
-			reloadShaders();
-		}
-
 		ImGui_ImplGlfwGL3_NewFrame();
 
-		
-
-		glDepthFunc(GL_LESS);
-
-		//Own implenetation, Render directly to screen
+		//glDepthFunc(GL_LESS);
 
 		//bonobo::setRenderTarget(testingFbo, 0);
 		glUseProgram(terrainShader->mId);
@@ -298,69 +178,20 @@ void Testing::run()
 		bonobo::setUniform(*terrainShader, "model_to_clip_matrix", cast<f32>(mCamera.GetWorldToClipMatrix()));
 		bonobo::setUniform(*terrainShader, "model_to_world_matrix", mat4f::Identity());
 		
-		
-
 		glBindVertexArray(vao);
 		bonobo::checkForErrors();
-		glDrawArrays(GL_TRIANGLES, 0, 4);
+		glDrawArrays(GL_POINTS, 0, 4);
 		bonobo::checkForErrors();
 		glBindVertexArray(0u);
 		bonobo::drawFullscreen(*terrainShader);
 		bonobo::checkForErrors();
 
-		printf("Camera Rotation no:1: %f, no:2: %f\n", (mCamera.mRotation).x, (mCamera.mRotation).y);
-		
-		
-		//
-		// Pass 1: Render scene into the g-buffer
-		//
+		//printf("Camera Rotation no:1: %f, no:2: %f\n", (mCamera.mRotation).x, (mCamera.mRotation).y);
 
-
-	
-	
-		
-	
-		
-		
-			//
-			// Pass 2.2: Accumulate light i contribution
-			
-		//
-		// Pass 3: Compute final image using both the g-buffer and  the light accumulation buffer
-		//
-		
-		GLStateInspection::CaptureSnapshot("Resolve Pass");
-
-
-
-
-		//
-		// Pass 4: Draw wireframe cones on top of the final image for debugging purposes
-		//
-		//glUseProgram(shadowMapShader->mId);;
-		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		//for (uint32_t i = 0u; i < LIGHTS_NB; ++i) {
-		//	bonobo::setUniform(*shadowMapShader, "model_to_clip_matrix", mCamera.GetWorldToClipMatrix() * lightTransforms[i].GetMatrix() * lightOffsetTransform.GetMatrix() * coneScaleTransform.GetMatrix());
-
-		//	glBindVertexArray(coneVao);
-		//	bonobo::checkForErrors();
-
-		//	glDrawArrays(GL_TRIANGLE_STRIP, 0, coneVerticesNb);
-
-		//	glBindVertexArray(0u);
-		//	bonobo::checkForErrors();
-		//}
-		//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-
-		//
-		// Output content of the g-buffer as well as of the shadowmap, for debugging purposes
-		//
-		
-
+		//GLStateInspection::CaptureSnapshot("Resolve Pass");
 		bonobo::checkForErrors();
 
-		GLStateInspection::View::Render();
+		//GLStateInspection::View::Render(); // Disabling this turns off the GLStateInspection console within the render window
 		//Log::View::Render();
 		ImGui::Render();
 
@@ -374,105 +205,4 @@ void Testing::run()
 	glDeleteVertexArrays(1, &vao);
 	bonobo::checkForErrors();
 	vao = 0u;
-}
-
-
-
-static GLuint loadCone(GLuint& vboId, GLsizei& verticesNb, bonobo::ShaderProgram* shader)
-{
-	verticesNb = 65;
-	float vertexArrayData[65 * 3] = {
-		0.f, 1.f, -1.f,
-		0.f, 0.f, 0.f,
-		0.38268f, 0.92388f, -1.f,
-		0.f, 0.f, 0.f,
-		0.70711f, 0.70711f, -1.f,
-		0.f, 0.f, 0.f,
-		0.92388f, 0.38268f, -1.f,
-		0.f, 0.f, 0.f,
-		1.f, 0.f, -1.f,
-		0.f, 0.f, 0.f,
-		0.92388f, -0.38268f, -1.f,
-		0.f, 0.f, 0.f,
-		0.70711f, -0.70711f, -1.f,
-		0.f, 0.f, 0.f,
-		0.38268f, -0.92388f, -1.f,
-		0.f, 0.f, 0.f,
-		0.f, -1.f, -1.f,
-		0.f, 0.f, 0.f,
-		-0.38268f, -0.92388f, -1.f,
-		0.f, 0.f, 0.f,
-		-0.70711f, -0.70711f, -1.f,
-		0.f, 0.f, 0.f,
-		-0.92388f, -0.38268f, -1.f,
-		0.f, 0.f, 0.f,
-		-1.f, 0.f, -1.f,
-		0.f, 0.f, 0.f,
-		-0.92388f, 0.38268f, -1.f,
-		0.f, 0.f, 0.f,
-		-0.70711f, 0.70711f, -1.f,
-		0.f, 0.f, 0.f,
-		-0.38268f, 0.92388f, -1.f,
-		0.f, 1.f, -1.f,
-		0.f, 1.f, -1.f,
-		0.38268f, 0.92388f, -1.f,
-		0.f, 1.f, -1.f,
-		0.70711f, 0.70711f, -1.f,
-		0.f, 0.f, -1.f,
-		0.92388f, 0.38268f, -1.f,
-		0.f, 0.f, -1.f,
-		1.f, 0.f, -1.f,
-		0.f, 0.f, -1.f,
-		0.92388f, -0.38268f, -1.f,
-		0.f, 0.f, -1.f,
-		0.70711f, -0.70711f, -1.f,
-		0.f, 0.f, -1.f,
-		0.38268f, -0.92388f, -1.f,
-		0.f, 0.f, -1.f,
-		0.f, -1.f, -1.f,
-		0.f, 0.f, -1.f,
-		-0.38268f, -0.92388f, -1.f,
-		0.f, 0.f, -1.f,
-		-0.70711f, -0.70711f, -1.f,
-		0.f, 0.f, -1.f,
-		-0.92388f, -0.38268f, -1.f,
-		0.f, 0.f, -1.f,
-		-1.f, 0.f, -1.f,
-		0.f, 0.f, -1.f,
-		-0.92388f, 0.38268f, -1.f,
-		0.f, 0.f, -1.f,
-		-0.70711f, 0.70711f, -1.f,
-		0.f, 0.f, -1.f,
-		-0.38268f, 0.92388f, -1.f,
-		0.f, 0.f, -1.f,
-		0.f, 1.f, -1.f,
-		0.f, 0.f, -1.f
-	};
-
-	GLuint vaoId = 0u;
-	GLint loc = glGetAttribLocation(shader->mId, "Vertex");
-	bonobo::checkForErrors();
-	glGenVertexArrays(1, &vaoId);
-	bonobo::checkForErrors();
-	glBindVertexArray(vaoId);
-	bonobo::checkForErrors();
-	{
-		glGenBuffers(1, &vboId);
-		bonobo::checkForErrors();
-		glBindBuffer(GL_ARRAY_BUFFER, vboId);
-		bonobo::checkForErrors();
-		glBufferData(GL_ARRAY_BUFFER, verticesNb * 3 * sizeof(float), vertexArrayData, GL_STATIC_DRAW);
-		bonobo::checkForErrors();
-
-		glVertexAttribPointer(loc, 3, GL_FLOAT, GL_FALSE, 0, 0);
-		bonobo::checkForErrors();
-		glEnableVertexAttribArray(loc);
-		bonobo::checkForErrors();
-	}
-	glBindVertexArray(0u);
-	bonobo::checkForErrors();
-	glBindBuffer(GL_ARRAY_BUFFER, 0u);
-	bonobo::checkForErrors();
-
-	return vaoId;
 }
